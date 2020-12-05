741663ab908226e96575fa0825a85e22818facfc
sdkei <sdkei0xb@gmail.com>
2020-12-05 22:40:46 +0900

    feat: Activity を追加。

---
+++ app/src/main/java/io/github/sdkei/loginmvvm/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.sdkei.loginmvvm.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.sdkei.loginmvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
