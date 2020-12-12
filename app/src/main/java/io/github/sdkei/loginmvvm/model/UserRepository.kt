b034c5a60ae6318f86e74279996c2de260199c8f
sdkei <sdkei0xb@gmail.com>
2020-12-12 13:22:52 +0900

    feat: UserRepository で排他制御を行うようにした。

--- app/src/main/java/io/github/sdkei/loginmvvm/model/UserRepository.kt
+++ app/src/main/java/io/github/sdkei/loginmvvm/model/UserRepository.kt
@@ -1,13 +1,16 @@
 package io.github.sdkei.loginmvvm.model
 
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 /** 登録ユーザーを管理するリポジトリー。 */
 object UserRepository {
 
+    private val mutex = Mutex()
+
     /** ユーザー名簿。 */
-    // 簡単のために Map にする。排他制御も省略する。本来はローカル DB やリモート API などとなる。
+    // 簡単のために Map にする。本来はローカル DB やリモート API などとなる。
     private val users: MutableMap<String, String> = mutableMapOf()
 
     init {
@@ -29,15 +32,15 @@ object UserRepository {
     suspend fun register(userId: String, password: String): Boolean {
         requireUserIdAndPassword(userId, password)
 
-        yield()
+        mutex.withLock {
+            users[userId]?.also {
+                return false
+            }
 
-        users[userId]?.also {
-            return false
+            users[userId] = password
+
+            return true
         }
-
-        users[userId] = password
-
-        return true
     }
 
     /**
@@ -48,9 +51,9 @@ object UserRepository {
     suspend fun authenticate(userId: String, password: String): Boolean {
         requireUserIdAndPassword(userId, password)
 
-        yield()
-
-        return users[userId] == password
+        mutex.withLock {
+            return users[userId] == password
+        }
     }
 
     /** 引数で与えられたユーザー ID とパスワードが適切なフォーマットであるかどうかを確認する。 */
